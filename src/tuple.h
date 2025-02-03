3935736ce3e48ce2a0174d476039f2f639fb91c3
Sandeep Vedavyasa Raveendra <svedavyasara@gmail.com>
2025-02-03 18:59:22 +0530

    custom tuple

---
+++ src/tuple.h
@@ -0,0 +1,77 @@
+#ifndef SVR_TUPLE
+#define SVR_TUPLE
+
+namespace svr
+{
+    template <typename T, typename... Ts>
+    struct tuple
+    {
+        tuple(const T &t, const Ts &...ts) : value(t), rest(ts...)
+        {
+        }
+
+        T value;
+        tuple<Ts...> rest;
+    };
+
+    template <typename T>
+    struct tuple<T>
+    {
+        tuple(const T &t) : value(t)
+        {
+        }
+
+        T value;
+    };
+
+    template <size_t N, typename T, typename... Ts>
+    struct nth_type : nth_type<N - 1, Ts...>
+    {
+        static_assert(N < sizeof...(Ts) + 1,
+                      "index out of bounds");
+    };
+
+    template <typename T, typename... Ts>
+    struct nth_type<0, T, Ts...>
+    {
+        using value_type = T;
+    };
+
+    template <size_t N>
+    struct getter
+    {
+        template <typename T, typename... Ts>
+        static nth_type<N, T, Ts...>::value_type &get(tuple<T, Ts...> &t)
+        {
+            return getter<N - 1>::get(t.rest);
+        }
+    };
+
+    template <>
+    struct getter<0>
+    {
+        template <typename T, typename... Ts>
+        static nth_type<0, T, Ts...>::value_type &get(tuple<T, Ts...> &t)
+        {
+            return t.value;
+        }
+    };
+
+    template <size_t N, typename... Ts>
+    typename nth_type<N, Ts...>::value_type &get(tuple<Ts...> &ts)
+    {
+        return getter<N>::get(ts);
+    }
+}
+
+#endif
+
+#include <iostream>
+
+void test()
+{
+    svr::tuple<int, double, char> three(1, 42.0, 'a');
+    std::cout << svr::get<0>(three) << "\n";
+    std::cout << svr::get<1>(three) << "\n";
+    std::cout << svr::get<2>(three) << "\n";
+}
